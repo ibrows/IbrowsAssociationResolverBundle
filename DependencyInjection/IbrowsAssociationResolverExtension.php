b63df12d617cbcdbda8f5d5c61d394dec956f833
Igor Weigel <igor.weigel@me.com>
2026-05-12 18:12:52 +0200

    Sf 7.4 fix: use DependencyInjection\Extension\Extension (HttpKernel variant deprecated)

--- DependencyInjection/IbrowsAssociationResolverExtension.php
+++ DependencyInjection/IbrowsAssociationResolverExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class IbrowsAssociationResolverExtension extends Extension
 {
